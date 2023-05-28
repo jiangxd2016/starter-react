e0d7ac49a32eff1a1d910a1fb9800a95fecf6572
jiangxd <jiangxd2016@gmail.com>
2023-05-28 17:56:21 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { ConfigProvider ,Empty} from 'antd';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+import zhCN from 'antd/locale/zh_CN';
+import 'antd/dist/reset.css';
+import './index.css';
+import { Provider } from 'react-redux';
+dayjs.locale('zh-cn');
+import store from './store';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Nodata from "@/assets/no-data.svg"
+
+const App = () => {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ConfigProvider locale={zhCN} renderEmpty={() => <Empty image={Nodata}> </Empty>}>
+       <Provider store={store}>
+        </Provider>
+    </ConfigProvider>
+  );
+};
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import Unocss from 'unocss/vite';
 import path from 'path';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
